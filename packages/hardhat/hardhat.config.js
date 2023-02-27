036c4e6eb64400d9f48ded2c66d644609c23a8b2
aditya choubey <72050163+adityachoubeyy@users.noreply.github.com>
2023-02-28 00:08:04 +0530

    Initialize dependabot/npm_and_yarn/http-cache-semantics-4.1.1

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -70,6 +70,7 @@ module.exports = {
     },
     mainnet: {
       url: "https://mainnet.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      //      url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXXXX/eth/mainnet", // <---- YOUR MORALIS ID! (not limited to infura)
       accounts: {
         mnemonic: mnemonic(),
       },
@@ -84,7 +85,7 @@ module.exports = {
       },
     },
     gnosis: {
-      url: "https://rpc.gnosischain.com/",
+      url: 'https://rpc.gnosischain.com/',
       gasPrice: 1000000000,
       accounts: {
         mnemonic: mnemonic(),
@@ -92,19 +93,21 @@ module.exports = {
     },
     polygon: {
       url: "https://polygon-rpc.com",
+      // url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXx/polygon/mainnet", // <---- YOUR MORALIS ID! (not limited to infura)
       accounts: {
         mnemonic: mnemonic(),
       },
     },
     goerli: {
       url: "https://goerli.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      //      url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXXXX/eth/goerli", // <---- YOUR MORALIS ID! (not limited to infura)
       accounts: {
         mnemonic: mnemonic(),
       },
     },
     sepolia: {
-      url: "https://rpc.sepolia.org",
-      // "https://sepolia.infura.io/v3/INFURA_ID",
+      url: "https://rpc.sepolia.org", // <---- YOUR INFURA ID! (or it won't work)
+      //      url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXXXX/eth/goerli", // <---- YOUR MORALIS ID! (not limited to infura)
       accounts: {
         mnemonic: mnemonic(),
       },
@@ -117,7 +120,7 @@ module.exports = {
       },
     },
     chiado: {
-      url: "https://rpc.chiadochain.net",
+      url: 'https://rpc.chiadochain.net',
       gasPrice: 1000000000,
       accounts: {
         mnemonic: mnemonic(),
@@ -139,6 +142,7 @@ module.exports = {
     },
     mumbai: {
       url: "https://rpc-mumbai.maticvigil.com",
+      // url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/polygon/mumbai", // <---- YOUR MORALIS ID! (not limited to infura)
       accounts: {
         mnemonic: mnemonic(),
       },
@@ -311,7 +315,6 @@ module.exports = {
       kovan: "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW",
       rinkeby: "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW",
       ropsten: "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW",
-      sepolia: "DNXJA8RX2Q3VZ4URQIWP7Z68CJXQZSC6AW",
       // add other network's API key here
     },
   },
