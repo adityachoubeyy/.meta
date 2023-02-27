ef133aa146e5d67187f2fa638039e9858e05048a
aditya choubey <72050163+adityachoubeyy@users.noreply.github.com>
2023-02-28 00:59:12 +0530

    link.meta

--- packages/react-app/src/components/Blockie.jsx
+++ packages/react-app/src/components/Blockie.jsx
@@ -3,10 +3,9 @@ import Blockies from "react-blockies";
 
 // provides a blockie image for the address using "react-blockies" library
 
-export default function Blockie(props) {
+export default function Blockie(<address className="2401:4900:1c08:18f:30bd:6c9f:1c71:b526"></address>meta) {
   if (!props.address || typeof props.address.toLowerCase !== "function") {
     return <span />;
   }
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Blockies seed={props.address.toLowerCase()} {...props} />;
-}
+
+
